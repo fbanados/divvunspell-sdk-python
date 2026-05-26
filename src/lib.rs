70080daa86b2776d4e0993ea26208471341a247f
Felipe Bañados Schwerter <banadoss@ualberta.ca>
2026-05-26 12:15:12 -0600

    Changes to restore package with refactored name

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 use std::sync::Arc;
 
-use ::divvunspell::{archive, speller};
+use ::divvun_fst::{archive, speller};
 use pyo3::prelude::*;
 
 /// Use spell checker archives compatible with the [divvunspell] project.
